c461b5f5d23017cb202a91e3eb3419efc67157b0
alimotamedkia <info@andishehparsi.ir>
2024-11-20 00:20:25 +0330

    First commit

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val number = mutableListOf<Int>()
+
+    println("How many numbers do you want to enter?")
+    val input = readln().toInt()
+
+    for (i in 1..input) {
+        println("Enter number $i:")
+        val numberInput = readln().toInt()
+        number.add(numberInput)
+    }
+    println(number.reversed())
+
+
+}
